9fb60c036f558b8a62b61f70227843bd3ee13b52
v-kbullen <v-kbullen@microsoft.com>
2025-01-24 12:03:51 -0500

    Update tde_demo.sql

--- tde_demo.sql
+++ tde_demo.sql
@@ -7,7 +7,7 @@ We grant You a nonexclusive, royalty-free right to use and modify the Sample Cod
 the object code form of the Sample Code, provided that You agree: ?
 (i) to not use Our name, logo, or trademarks to market Your software product in which the Sample Code is embedded; ?
 (ii) to include a valid copyright notice on Your software product in which the Sample Code is embedded; and ?
-(iii) to indemnify, hold harmless, and defend Us and Our suppliers from and against any claims or lawsuits, including attorneys’ fees, 
+(iii) to indemnify, hold harmless, and defend Us and Our suppliers from and against any claims or lawsuits, including attorneysâ€™ fees, 
 that arise or result from the use or distribution of the Sample Code. ?
 Please note: None of the conditions outlined in the disclaimer above will supersede the terms and conditions 
 contained within the Premier Customer Services Description. ?
@@ -129,7 +129,7 @@ USE master
 GO
 IF NOT EXISTS (SELECT * 
                FROM sys.certificates 
-			   WHERE name = 'TDETestCert8675309')
+	       WHERE name = 'TDETestCert8675309')
 BEGIN
 	DROP CERTIFICATE TDETestCert8675309;
 END
